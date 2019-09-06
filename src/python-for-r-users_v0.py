2015748bc3ab7a7f18ec05a20cadbef9c1749356
godsylla <sylvi.tran@gmail.com>
2019-09-05 21:34:38 -0700

    slideshow

---
+++ src/python-for-r-users_v0.py
@@ -0,0 +1,12 @@
+# Importing packages 
+# R : `library('package_name')`
+import pandas as pd
+import numpy as np
+
+# sklearn has built-in toy datasets the same way R does
+# https://scikit-learn.org/stable/datasets/index.html
+from sklearn.datasets import load_boston
+boston = load_boston()
+
+
+
